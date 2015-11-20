1e8bf91eb6677abde31300e8ab254ec03110f8a3
shramee <shramee.srivastav@gmail.com>
2015-11-20 13:08:07 +0530

    File handling and only one css tab

--- customisations.php
+++ customisations.php
@@ -62,6 +62,11 @@ class Pootle_Customisations {
 					width: 750px;
 					height: 520px;
 				}
+				.large img.emoji {
+					height: 1.6em!important;
+					width: 1.6em!important;
+					vertical-align: middle!important;
+				}
 			</style>
 
 			<div id="icon-themes" class="icon32"></div>
@@ -79,8 +84,10 @@ class Pootle_Customisations {
 
 			<h2 class="nav-tab-wrapper">
 				<a href="?page=pootle_customizations&tab=css" class="nav-tab <?php echo $active_tab == 'css' ? 'nav-tab-active' : ''; ?>">CSS</a>
+				<?php if ( apply_filters( 'pootle_customizations_dev', false ) ) { ?>
 				<a href="?page=pootle_customizations&tab=mob_css" class="nav-tab <?php echo $active_tab == 'mob_css' ? 'nav-tab-active' : ''; ?>">Mobile CSS</a>
 				<a href="?page=pootle_customizations&tab=desk_css" class="nav-tab <?php echo $active_tab == 'desk_css' ? 'nav-tab-active' : ''; ?>">Desktop CSS</a>
+				<?php } ?>
 				<a href="?page=pootle_customizations&tab=php" class="nav-tab <?php echo $active_tab == 'php' ? 'nav-tab-active' : ''; ?>">Functions</a>
 			</h2>
 
@@ -164,6 +171,7 @@ class Pootle_Customisations {
 	public function render_section_css() {
 		$value = get_option( 'pootle_custo_css' );
 		?>
+		<p>Head over to <a href="http://www.pootlepress.com/customizations">http://www.pootlepress.com/customizations</a> to grab some awesome media query snippets <span class="large"><?php echo convert_smilies( ";)" ); ?></span>.</p>
 		<textarea class="hidden" name="pootle_custo_css"><?php echo $value; ?></textarea>
 		<div id="editor"><?php echo $value ?></div>
 		<?php
@@ -172,6 +180,7 @@ class Pootle_Customisations {
 	public function render_section_mob_css() {
 		$value = get_option( 'pootle_custo_mob_css' );
 		?>
+		<p>CSS you put here will be applied to your website only on iPhones and other small mobile devices.</p>
 		<textarea class="hidden" name="pootle_custo_mob_css"><?php echo $value; ?></textarea>
 		<div id="editor"><?php echo $value ?></div>
 		<?php
@@ -180,6 +189,7 @@ class Pootle_Customisations {
 	public function render_section_desk_css() {
 		$value = get_option( 'pootle_custo_desk_css' );
 		?>
+		<p>CSS you put here will be applied to your website on iPads and other larger mobile devices and desktops.</p>
 		<textarea class="hidden" name="pootle_custo_desk_css"><?php echo $value; ?></textarea>
 		<div id="editor"><?php echo $value ?></div>
 		<?php
